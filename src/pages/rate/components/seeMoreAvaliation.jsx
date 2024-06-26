f466436ca63b70ab5eefa4c9d58aea03d524c9d7
juangarciadelima <juangarciadelima884@gmail.com>
2024-06-26 17:31:56 -0300

    fix: correcting see more avaliation card

--- src/pages/rate/components/seeMoreAvaliation.jsx
+++ src/pages/rate/components/seeMoreAvaliation.jsx
@@ -90,7 +90,7 @@ export default function SeeMoreAvaliation({
             borderLeft="6px solid"
             borderLeftColor="#971520"
           >
-            <ModalHeader padding="0px">
+            <ModalHeader paddingBottom="10px">
               <Container
                 bg="#1c222b"
                 minW="100%"
@@ -100,9 +100,12 @@ export default function SeeMoreAvaliation({
                 borderTopRightRadius="8px"
                 color="white"
               >
-                <Heading size="ms">
-                  Avaliação feita por: {formattingText(avaliation.reviewer)}
-                </Heading>
+                <Text display="flex" gap="10px" fontWeight="400">
+                  Avaliação feita por:
+                  <Heading size="ms">
+                    {formattingText(avaliation.reviewer)}
+                  </Heading>
+                </Text>
                 <IconButton
                   onClick={onClose}
                   bg="transparent"
@@ -119,164 +122,172 @@ export default function SeeMoreAvaliation({
                 padding="0px"
                 margin="0px"
                 marginBottom="20px"
+                flexDir="column"
+                alignItems="center"
+                gap="20px"
               >
-                <Container padding="0px" fontSize="18px">
-                  <Flex>
+                <Container display="flex" flexDir="row">
+                  <Container padding="0px" fontSize="18px">
+                    <Flex>
+                      <strong>
+                        <Text marginRight="8px">Pessoa avaliada:</Text>
+                      </strong>
+                      {formattingText(avaliation.reviewed)}
+                    </Flex>
+                    <Button
+                      bgColor="#700e17"
+                      color="white"
+                      marginTop="10px"
+                      onClick={showObservations}
+                      _hover={{}}
+                      _active={{ bgColor: "#520a11" }}
+                    >
+                      {!alternanceObservationandQuestions
+                        ? "Mostrar Observação"
+                        : "Mostrar Perguntas"}
+                    </Button>
+                  </Container>
+                  <Container width="auto" padding="0px" fontSize="18px">
+                    <Flex alignItems="center" fontSize="20px">
+                      <CalendarIcon marginRight="5px" />
+                      <strong>
+                        <Text>{avaliation.date}</Text>
+                      </strong>
+                    </Flex>
                     <strong>
-                      <Text marginRight="8px">Pessoa avaliada:</Text>
+                      <Text whiteSpace="nowrap" marginTop="10px">
+                        Média dos Ratings:{" "}
+                        {getAverageRating(
+                          filterValidRatings(avaliation.questions)
+                        )}
+                      </Text>
                     </strong>
-                    {formattingText(avaliation.reviewed)}
-                  </Flex>
-                  <Button
-                    bgColor="#700e17"
-                    color="white"
-                    marginTop="10px"
-                    onClick={showObservations}
-                    _hover={{}}
-                    _active={{ bgColor: "#520a11" }}
-                  >
-                    {!alternanceObservationandQuestions
-                      ? "Mostrar Observação"
-                      : "Mostrar Perguntas"}
-                  </Button>
+                  </Container>
                 </Container>
-                <Container width="auto" padding="0px" fontSize="18px">
-                  <Flex alignItems="center" fontSize="20px">
-                    <CalendarIcon marginRight="5px" />
-                    <strong>
-                      <Text>{avaliation.date}</Text>
-                    </strong>
-                  </Flex>
-                  <strong>
-                    <Text whiteSpace="nowrap" marginTop="10px">
-                      Média dos Ratings:{" "}
-                      {getAverageRating(
-                        filterValidRatings(avaliation.questions)
-                      )}
-                    </Text>
-                  </strong>
-                </Container>
-              </Container>
-              {alternanceObservationandQuestions === false ? (
-                <Container
-                  width="100%"
-                  minHeight="300px"
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="center"
-                  flexDirection="column"
-                  position="absolute"
-                >
+                {alternanceObservationandQuestions === false ? (
                   <Container
+                    width="100%"
+                    display="flex"
+                    flexDir="column"
+                    alignItems="center"
+                    justifyContent="center"
+                    flexDirection="column"
+                    paddingLeft="0px"
+                  >
+                    <Container
+                      display="flex"
+                      alignItems="center"
+                      justifyContent="center"
+                      flexDirection="column"
+                      top="0px"
+                    >
+                      <Container textAlign="center">
+                        <Heading fontSize="40px">
+                          {filteredQuestions[currentQuestion].question}
+                        </Heading>
+                        <Text fontSize="22px">
+                          {
+                            filteredQuestions[currentQuestion]
+                              .questionDescription
+                          }
+                        </Text>
+                      </Container>
+                      <div style={{ display: "flex" }}>
+                        {[...Array(parseInt(5))].map((_, i) => {
+                          const ratingValue = i + 1;
+
+                          return (
+                            <label key={ratingValue}>
+                              <input
+                                type="radio"
+                                style={{ opacity: "0" }}
+                                name="rating"
+                                value={ratingValue}
+                              />
+                              <TiStar
+                                className="star"
+                                color={
+                                  ratingValue <= rating
+                                    ? "#971520"
+                                    : "#ffffff2b"
+                                }
+                                size={50}
+                              />
+                              <Text
+                                textAlign="center"
+                                position="relative"
+                                bottom="10px"
+                                color="white"
+                              >
+                                <strong>{ratingValue}</strong>
+                              </Text>
+                            </label>
+                          );
+                        })}
+                      </div>
+                    </Container>
+                    <Container
+                      padding="0px"
+                      display="flex"
+                      justifyContent="start"
+                      width="100%"
+                      flexDirection="column"
+                      bottom="20px"
+                      paddingBottom="20px"
+                    >
+                      <div>
+                        <strong>Justificativa</strong>
+                      </div>
+                      <StyledTextarea
+                        required
+                        readOnly
+                        defaultValue={
+                          justifications || "Esta questão não foi justificada"
+                        }
+                      />
+                    </Container>
+                  </Container>
+                ) : (
+                  <Container
+                    width="100%"
+                    minHeight="300px"
                     display="flex"
                     alignItems="center"
-                    justifyContent="space-between"
-                    flexDirection="column"
-                    position="absolute"
-                    top="0px"
-                  >
-                    <Container textAlign="center">
-                      <Heading fontSize="40px">
-                        {filteredQuestions[currentQuestion].question}
-                      </Heading>
-                      <Text fontSize="22px">
-                        {filteredQuestions[currentQuestion].questionDescription}
-                      </Text>
-                    </Container>
-                    <div style={{ display: "flex" }}>
-                      {[...Array(parseInt(5))].map((_, i) => {
-                        const ratingValue = i + 1;
-
-                        return (
-                          <label key={ratingValue}>
-                            <input
-                              type="radio"
-                              style={{ opacity: "0" }}
-                              name="rating"
-                              value={ratingValue}
-                            />
-                            <TiStar
-                              className="star"
-                              color={
-                                ratingValue <= rating ? "#971520" : "#ffffff2b"
-                              }
-                              size={50}
-                            />
-                            <Text
-                              textAlign="center"
-                              position="relative"
-                              bottom="10px"
-                              color="white"
-                            >
-                              <strong>{ratingValue}</strong>
-                            </Text>
-                          </label>
-                        );
-                      })}
-                    </div>
-                  </Container>
-
-                  <Container
-                    padding="0px"
-                    display="flex"
                     justifyContent="center"
-                    width="100%"
                     flexDirection="column"
                     position="absolute"
-                    bottom="20px"
+                    top="128px"
                   >
-                    <div>
-                      <strong>Justificativa</strong>
-                    </div>
-                    <StyledTextarea
-                      required
+                    <Heading fontSize="40px">Observação</Heading>
+                    <Textarea
+                      resize="none"
                       readOnly
-                      defaultValue={
-                        justifications || "Esta questão não foi justificada"
-                      }
+                      fontSize="20px"
+                      height="200px"
+                      marginTop="25px"
+                      focusBorderColor="#ffffff"
+                      overflow="hidden"
+                      overflowY="auto"
+                      css={{
+                        "&::-webkit-scrollbar": {
+                          borderRadius: "10px",
+                          width: "6px",
+                          direction: "rtl",
+                        },
+                        "&::-webkit-scrollbar-thumb": {
+                          backgroundColor: "#700e17",
+                          borderRadius: "10px",
+                          width: "6px",
+                        },
+                        "&::-webkit-scrollbar-thumb:hover": {
+                          backgroundColor: "#86111c",
+                        },
+                      }}
+                      defaultValue={getObservation(avaliation.questions).rating}
                     />
                   </Container>
-                </Container>
-              ) : (
-                <Container
-                  width="100%"
-                  minHeight="300px"
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="center"
-                  flexDirection="column"
-                  position="absolute"
-                  top="128px"
-                >
-                  <Heading fontSize="40px">Observação</Heading>
-                  <Textarea
-                    resize="none"
-                    readOnly
-                    fontSize="20px"
-                    height="200px"
-                    marginTop="25px"
-                    focusBorderColor="#ffffff"
-                    overflow="hidden"
-                    overflowY="auto"
-                    css={{
-                      "&::-webkit-scrollbar": {
-                        borderRadius: "10px",
-                        width: "6px",
-                        direction: "rtl",
-                      },
-                      "&::-webkit-scrollbar-thumb": {
-                        backgroundColor: "#700e17",
-                        borderRadius: "10px",
-                        width: "6px",
-                      },
-                      "&::-webkit-scrollbar-thumb:hover": {
-                        backgroundColor: "#86111c",
-                      },
-                    }}
-                    defaultValue={getObservation(avaliation.questions).rating}
-                  />
-                </Container>
-              )}
+                )}
+              </Container>
             </ModalBody>
             <ModalFooter
               display="flex"
@@ -286,7 +297,7 @@ export default function SeeMoreAvaliation({
               paddingLeft="20px"
               minW="100%"
             >
-              {alternanceObservationandQuestions === false && (
+              {!alternanceObservationandQuestions && (
                 <>
                   <Container
                     padding="0px"
@@ -295,6 +306,7 @@ export default function SeeMoreAvaliation({
                     alignItems="center"
                     justifyContent="space-between"
                     maxW="100px"
+                    paddingBottom="10px"
                   >
                     {currentQuestion !== 0 && (
                       <Button
